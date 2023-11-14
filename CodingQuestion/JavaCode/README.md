3670a33388f02e2b1db8b4e7222e9a18445b737a
West-Circle <yuan_0006@hotmail.com>
2023-11-14 13:07:36 +0800

    add basic sorting, string, thread code

--- CodingQuestion/JavaCode/README.md
+++ CodingQuestion/JavaCode/README.md
@@ -5,3 +5,5 @@
 # Program Files
 - ArrayIterator : using array to print elements and add elements into list then print by using iterator
 - balanceBracket : using stack to check the parentheses is balance or not.
+- Sorting : sorting algorithm such as bubblesort, quicksort, mergesort and also binary search for index in array
+- StringTest : compare the string using == and equals()
---
+++ CodingQuestion/JavaCode/ThreadTest.java
@@ -0,0 +1,33 @@
+package JavaCode;
+
+class ThreadRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("ThreadRunnable1 is running");
+    }
+}
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread t1 is running");
+        });
+        Thread t2 = new Thread(() -> {
+            System.out.println("Thread t2 is running");
+        });
+        Thread t3 = new Thread(() -> {
+            System.out.println("Thread t3 is running");
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+        // using runnable
+        Thread tr = new Thread(new ThreadRunnable() {
+            @Override
+            public void run() {
+                System.out.println("ThreadRunnable2 is running");
+            }
+        });
+        tr.start();
+    }
+}
